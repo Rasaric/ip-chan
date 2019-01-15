1ed9b2949e759fcae62c6e1d40ad82845ba82520
Rasaric <santiago@smarticaweb.com>
2019-01-15 14:46:29 -0400

    dice roller

--- index.js
+++ index.js
@@ -193,7 +193,7 @@ bot.on("message", async message => {
         let diceAmount = diceValues[0]//store amount of dice to roll
         let diceTypenMod = diceValues[1]//stores 20+3
         message.channel.send(diceTypenMod + ' this should be 20+3, all good up to here');
-        return function diceTypeSetter() {
+        function diceTypeSetter() {
             if (diceTypenMod.includes('+')){
                 let newDiceTypenMod = diceTypenMod.split('+');//split 20 + 3
                 let diceType = newDiceTypenMod[0]//stores 20
@@ -206,13 +206,14 @@ bot.on("message", async message => {
                 let diceType = diceTypenMod;//if no mod is passed
             }
         }
+        return diceTypeSetter();
         message.channel.send(diceType + 'this should be 20');
         message.channel.send(diceMod + 'this should be 3');
 
         let diceRoll = 0;        
         //generate random number and multiply by diceType
 
-        return function diceRoller() {
+        function diceRoller() {
             for (var i = 1;  i < diceAmount; i++) {//roll new numbers for amount passed in
                 let diceRoll = diceRoll + Math.floor(Math.random() * diceType);
                 
