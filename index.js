9e44ed81c04a7cac58621b3e3eb80bbf99220eab
Rasaric <santiago@smarticaweb.com>
2019-01-15 14:53:24 -0400

    dice roller

--- index.js
+++ index.js
@@ -195,17 +195,16 @@ bot.on("message", async message => {
         message.channel.send(diceTypenMod + ' this should be 20+3, all good up to here');
         function diceTypeSetter() {
             if (diceTypenMod.includes('+')){
-                let newDiceTypenMod = diceTypenMod.split('+');//split 20 + 3
-                let diceType = newDiceTypenMod[0]//stores 20
-                let diceMod = newDiceTypenMod[1]//stores 3
+                newDiceTypenMod = diceTypenMod.split('+');//split 20 + 3
+                diceType = newDiceTypenMod[0]//stores 20
+                diceMod = newDiceTypenMod[1]//stores 3
 
                 message.channel.send("if you see this something is not really right");
 
             } else {
-                let diceType = diceTypenMod;//if no mod is passed
+                diceType = diceTypenMod;//if no mod is passed
             }
         }
-        return diceTypeSetter();
         message.channel.send(diceType + 'this should be 20');
         message.channel.send(diceMod + 'this should be 3');
       
@@ -213,7 +212,7 @@ bot.on("message", async message => {
 
         function diceRoller() {
             for (var i = 1;  i < diceAmount; i++) {//roll new numbers for amount passed in
-                let diceRoll = diceRoll + Math.floor(Math.random() * diceType);
+                diceRoll = diceRoll + Math.floor(Math.random() * diceType);
                 
             }
         }
