11481600520ecf8c0c04378b1e5eb48164ff2680
Rasaric <santiago@smarticaweb.com>
2019-01-15 14:34:02 -0400

    dice roller

--- index.js
+++ index.js
@@ -190,44 +190,47 @@ bot.on("message", async message => {
         let dice = message.content.split(' ');//remove command
         let newDice = dice[1]
         let diceValues= newDice.split('d');//split 1 d 20+3
-        let diceAmount = number(diceValues[0])//store amount of dice to roll
+        let diceAmount = diceValues[0]//store amount of dice to roll
         let diceTypenMod = diceValues[1]//stores 20+3
 
         let diceType = 0
         let diceMod = 0
         diceTypeSetter();
-        function diceTypeSetter() {
+        return function diceTypeSetter() {
             if (diceTypenMod.includes('+')){
                 let newDiceTypenMod = diceTypenMod.split('+');//split 20 + 3
-                let diceType = number(newDiceTypenMod[0])//stores 20
-                let diceMod = number(newDiceTypenMod[1])//stores 3
+                let diceType = newDiceTypenMod[0]//stores 20
+                let diceMod = newDiceTypenMod[1]//stores 3
 
                 message.channel.send(diceType + 'this should be 20');
                 message.channel.send(diceMod + 'this should be 3');
 
             } else {
-                let diceType = number(diceTypenMod);//if no mod is passed
+                let diceType = diceTypenMod;//if no mod is passed
             }
         }
-        return diceTypeSetter
         message.channel.send(diceType + 'this should be 20');
         message.channel.send(diceMod + 'this should be 3');
 
         let diceRoll = 0;        
         //generate random number and multiply by diceType
 
-        for (var i = 1;  i < diceAmount; i++) {//roll new numbers for amount passed in
-            let diceRoll = diceRoll + Math.floor(Math.random() * diceType);
-            let diceResult = diceRoll + diceMod//add modifier after rolling
-            //send results
-            if (diceResult == 1){
-                message.channel.send('you rolled a nat **' + diceResult + '**, hope the DM takes pity on you.');
-            } else if (diceResult == 20) {
-                message.channel.send('you rolled a nat **' + diceResult + '**, Critical Hit!');
+        return function diceRoller() {
+            for (var i = 1;  i < diceAmount; i++) {//roll new numbers for amount passed in
+                let diceRoll = diceRoll + Math.floor(Math.random() * diceType);
+                
             }
-            else {
-                message.channel.send('you rolled **' +  dice + '** and got **' + diceResult + '**');
-            }
+        }
+
+        let diceResult = diceRoll + diceMod//add modifier after rolling
+        
+        //send results
+        if (diceResult == 1){
+            message.channel.send('you rolled a nat **' + diceResult + '**, hope the DM takes pity on you.');
+        } else if (diceResult == 20) {
+            message.channel.send('you rolled a nat **' + diceResult + '**, Critical Hit!');
+        } else {
+            message.channel.send('you rolled **' +  dice + '** and got **' + diceResult + '**');
         }
     }
         
