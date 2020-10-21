3cbd3f4f7a7811cb542d033d4116062517aa605b
Rasaric <theguyindarkglasses@gmail.com>
2020-10-20 20:13:43 -0400

    remove bot token

--- index.js
+++ index.js
@@ -57,4 +57,3 @@ bot.on("message", async message => {
 		responses(message, bot, waifus);
 });
 bot.login(process.env.BOT_TOKEN);
-bot.login("NTA5Nzk4NTAwMDY2OTE4NDAx.DwAezA.L5KIcvtP6X4Ipj8QyG5ksjJLTdk");
